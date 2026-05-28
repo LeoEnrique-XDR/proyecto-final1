3f9b56a6abacb1f13bcfaa2256b02b13967bb4a4
Leonel <leonelxdr10@gmail.com>
2026-05-28 12:18:09 -0400

    Preguntas y conexion a la base datos

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+    const BASE_URL="http://localhost/blog/";
+    //const BASE_URL="https://proyectocgs.com/";
+    const HOST="localhost";
+    const DB="miblog";
+    const USER="root";
+    const PASSWORD="";
+?>
---
+++ modelos/Conexion.php
@@ -0,0 +1,10 @@
+<?php
+class Conexion{
+    static public function conectar(){
+        $conexion=new PDO("mysql:host=localhost;dbname=miblog","root","");
+        return $conexion;
+    }
+}
+
+
+?>
